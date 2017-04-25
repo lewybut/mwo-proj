7d2c111b73660cfa8ec01c8f3987a5bf5ae7ae4e
dawJar <dawid.jaracz@gmail.com>
2017-04-25 16:15:50 +0200

    cleaning

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,9 +3,11 @@ import { routerReducer as routing } from 'react-router-redux';
 import { navigation } from './navigation';
 import { home } from './home';
 import { form } from './form';
+import { login } from './login';
 
 const rootReducer = combineReducers({
     navigation,
+    login,
     home,
     form,
     routing
